d333b55d6bfb44435ed6e943c7829e66b00f8aac
luniki <luniki@gmail.com>
2014-07-11 11:42:10 +0200

    show up to 5 next dates but all of today

--- models/quickdail.php
+++ models/quickdail.php
@@ -20,8 +20,6 @@ class Quickdail {
 
     static function get_next_courses($user_id)
     {
-        $ret = array();
-
         // get list of all my courses
         $stmt = \DBManager::get()->prepare('SELECT DISTINCT Seminar_id
             FROM seminar_user
@@ -31,19 +29,23 @@ class Quickdail {
         $seminar_ids = $stmt->fetchAll(\PDO::FETCH_COLUMN);
 
         // get upcoming events
-        $stmt_next = \DBManager::get()->prepare('SELECT termin_id FROM termine
+        $stmt_next = \DBManager::get()->prepare('SELECT termin_id, date FROM termine
             WHERE range_id IN (:seminar_ids)
                 AND date >= (UNIX_TIMESTAMP() - 3600)
-            ORDER BY date ASC LIMIT 3');
+            ORDER BY date ASC LIMIT 100');
         $stmt_next->bindValue(':seminar_ids', $seminar_ids, \StudipPDO::PARAM_ARRAY);
 
         $stmt_next->execute();
 
-        // return upcoming events
-        while ($termin_id = $stmt_next->fetchColumn()) {
-            $ret[] = new \SingleDate($termin_id);
-        }
+        $termine = $stmt_next->fetchAll();
 
+        // return all upcoming events of today and up to 5 more
+        $ret = array();
+        $upto = 5;
+        $midnight = intval(1 + time() / 86400) * 86400;
+        for ($i = 0, $len = sizeof($termine); $i < $len && ($termine[$i]['date'] < $midnight || $i < $upto); $i++) {
+            $ret[] = new \SingleDate($termine[$i]['termin_id']);
+        }
         return $ret;
     }
 }
