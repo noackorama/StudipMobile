ee357d515d5fb6086210c5644639d1ab8e5233ab
luniki <luniki@gmail.com>
2014-07-11 11:16:51 +0200

    add partial template of next sessions

--- views/courses/show.php
+++ views/courses/show.php
@@ -16,51 +16,7 @@ $resources_locations = array_filter($resources, function ($resource) {
 <? } ?>
 
 
-<ul data-role="inlinelistview" data-inset="true">
-  <? if (count($next_dates)) : ?>
-
-
-  <li data-role="list-divider" data-theme="c">
-    Nächster Termin
-  </li>
-
-  <? foreach($next_dates as $next) { ?>
-  <li data-theme="<?= ($is_ex = $next->isExTermin()) ? 'e' : ''?>">
-
-    <?= $next->toString() ?>
-    <?= Studip\Mobile\Helper::out($next->getRoom())?>
-    <? $location = reset(array_filter($resources, function ($r) use ($next) { return $r["id"] == $next->resource_id; })) ?>
-
-    <? if ($location) { ?>
-      <span class="ui-li-aside">
-        <a href="<?= $controller->url_for("courses/show_map", $course->id) ?>" class="externallink" data-ajax="false">
-          <?= $location["description"] ?>
-        </a>
-      </span>
-    <? } ?>
-
-    <? if ($is_ex) : ?>
-      <i>Fällt aus! (Kommentar: <?= Studip\Mobile\Helper::out($next->getComment())?>)</i>
-    <? endif ?>
-
-  </li>
-  <? } ?>
-
-  <li data-rel="inline">
-      <a href="#inline-dates">Alle Termine</a>
-  </li>
-
-  <li id="inline-dates">
-    <div>
-        <? $sem = Seminar::getInstance($course->id) ?>
-        <?= nl2br(Studip\Mobile\Helper::out($sem->getDatesExport())) ?>
-    </div>
-
-  </li>
-
-  <? endif ?>
-</ul>
-
+<?= $this->render_partial('courses/_next_sessions') ?>
 
 <? if ($course->metadate) { ?>
 
@@ -75,7 +31,7 @@ $resources_locations = array_filter($resources, function ($resource) {
 
     <? if (strlen($misc = trim($this->render_partial('courses/_show_misc')))) : ?>
         <div data-role="collapsible" data-theme="c" data-content-theme="d">
-          <h3>Weiteres</h3>
+          <h3>Details</h3>
           <?= $misc ?>
         </div>
     <? endif ?>
