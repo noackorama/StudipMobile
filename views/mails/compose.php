773c509ca644a74f10a5ba2bdf238b741933f78f
luniki <luniki@gmail.com>
2014-11-20 15:14:22 +0100

    migrate field container

--- views/mails/compose.php
+++ views/mails/compose.php
@@ -17,7 +17,6 @@ STUDIP.Mobile.bootstraps.contacts = <?= $contacts ? json_encode($contacts) : 'nu
 <ul id="composer" data-role="listview" data-theme=d>
 
   <li class=recipients>
-    <div data-role="fieldcontain">
       <label for=rec-search>
         Empfänger: <span class=required>*</span>
       </label>
@@ -33,6 +32,7 @@ STUDIP.Mobile.bootstraps.contacts = <?= $contacts ? json_encode($contacts) : 'nu
       <? endif ?>
 
       <ul class=selected data-role=listview data-inset=false data-icon=minus></ul>
+    <div class="ui-field-contain">
 
       <input id=rec-search type=search data-clear-btn=true
              data-corners=false placeholder="Suchen">
@@ -40,7 +40,7 @@ STUDIP.Mobile.bootstraps.contacts = <?= $contacts ? json_encode($contacts) : 'nu
   </li>
 
   <li class=subject>
-    <div data-role="fieldcontain">
+    <div class="ui-field-contain">
       <label for=rec-subject> Betreff: <span class=required>*</span> </label>
       <input id=rec-subject name=subject type=text required
              value="<?= isset($mail['subject']) ? $this->out($mail['subject']) : '' ?>">
@@ -49,7 +49,7 @@ STUDIP.Mobile.bootstraps.contacts = <?= $contacts ? json_encode($contacts) : 'nu
 
 
   <li class=message>
-    <div data-role="fieldcontain">
+    <div class="ui-field-contain">
       <label for=rec-message> Nachricht: </label>
       <textarea id=rec-message name=message></textarea>
     </div>
